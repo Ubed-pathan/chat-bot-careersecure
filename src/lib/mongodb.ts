fccd1c98c630eeb82025b5bb0d14888b12ac5630
Ubed-pathan <ubedpathan818@gmail.com>
2025-06-05 15:12:33 +0530

    assignment completed

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,16 +1,13 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
-const options = {};
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
 if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your MongoDB URI to .env.local");
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+const client = new MongoClient(uri, options);
+const clientPromise = client.connect();
 
 export default clientPromise;
